38df56e70a3221945af6b9376ab5f5102f7288ad
laissa-albuquerquer <laissaalbuquerque@gmail.com>
2024-04-25 17:27:08 -0300

    alterações no design da tela de vídeo e alguns ajustes

--- lib/views/viewsForYears/kids_videos.dart
+++ lib/views/viewsForYears/kids_videos.dart
@@ -20,12 +20,21 @@ class KidsVideoScreen extends StatefulWidget {
 class _KidsVideoScreen extends State<KidsVideoScreen> {
   String? userImageUrl;
   String? userNickname;
+  bool _showImage = true;
+  final ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
     _loadNickname();
     _loadAvatar();
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 
   Future<void> _loadNickname() async {
@@ -54,6 +63,18 @@ class _KidsVideoScreen extends State<KidsVideoScreen> {
     return '';
   }
 
+  void _onScroll() {
+    if (_scrollController.offset > 1 && _showImage) {
+      setState(() {
+        _showImage = false;
+      });
+    } else if (_scrollController.offset <= 0 && !_showImage) {
+      setState(() {
+        _showImage = true;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -73,25 +94,106 @@ class _KidsVideoScreen extends State<KidsVideoScreen> {
                   Color(0xFF29084d),
                   Color(0xFF300558),
                   Color(0xFF380063),
-                ]), // Borda arredondada
+                ]),
           ),
         ),
         Image.asset('assets/images/stars-two.png', fit: BoxFit.fitHeight),
         Center(
           child: ListView(
+            controller: _scrollController,
             padding: const EdgeInsets.all(20),
             children: [
               Column(
                 children: [
-                  Container(
-                    
+                  AnimatedOpacity(
+                    opacity: _showImage ? 1.0 : 0.0,
+                    duration: const Duration(milliseconds: 200),
+                    child: Container(
+                      width: 380,
+                      height: 100,
+                      margin: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+                      decoration: BoxDecoration(
+                        borderRadius:
+                        const BorderRadius.all(Radius.circular(20.0)),
+                        border: Border.all(color: Colors.transparent, width: 3.0),
+                        gradient: const LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment(0.8, 1),
+                          colors: <Color>[
+                            Color(0xFF0e0019),
+                            Color(0xFF150423),
+                            Color(0xFF19082d),
+                            Color(0xFF1d0a38),
+                            Color(0xFF230943),
+                            Color(0xFF29084d),
+                            Color(0xFF300558),
+                            Color(0xFF380063),
+                          ],
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.fromLTRB(125, 4, 12, 4),
+                            child: Text(
+                              '"Olá ${_getNickname(userNickname ?? '')}! Aqui você vai encontrar '
+                                  'diversos vídeos que vão te ajudar nessa jornada de descobrir sobre a  '
+                                  'galáxia que habita em você."',
+                              textAlign: TextAlign.justify,
+                              style: const TextStyle(
+                                fontSize: 14,
+                                color: Colors.white,
+                                fontFamily: 'KleeOne',
+                                decoration: TextDecoration.none,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
                   )
                 ],
               ),
-              VideoPlayerView(
+              const SizedBox(height: 22),
+              const VideoPlayerView(
                   url: 'assets/videos/desbravando-nosso-corpo-H1.mp4',
                   nameVideo: 'Desbravando nosso corpo'),
+              const SizedBox(height: 28),
             ],
+          ),
+        ),
+        Positioned(
+          top: 18,
+          left: 0,
+          child: AnimatedOpacity(
+            opacity: _showImage ? 1.0 : 0.0,
+            duration: const Duration(milliseconds: 200),
+            child: Image.asset(
+              _getUserImageProvider(
+                  userImageUrl ?? 'assets/images/default_user.png'),
+              width: 150,
+              height: 150,
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 32,
+          left: 20,
+          child: FloatingActionButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            backgroundColor: const Color(0xffBFCAFF),
+            shape: const CircleBorder(),
+            child: const Icon(Icons.keyboard_arrow_left, color: Color(0xff37035F), size: 32),// Cor de fundo do botão
           ),
         ),
       ],
